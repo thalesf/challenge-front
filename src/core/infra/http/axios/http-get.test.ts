9c120cc5fa3867cc40166a97a632984f3c59fc87
Thales <thales.ferreira@volanty.com>
2021-04-27 18:21:21 -0300

    Add: jest config and file structures

---
+++ src/core/infra/http/axios/http-get.test.ts
@@ -0,0 +1,5 @@
+describe('Http Client Test', () => {
+  test('Should fetch user Luke Skywalker', async () => {
+
+  }
+})
---
+++ src/core/usecases/load-user.tsx
@@ -0,0 +1,3 @@
+export interface loadUser {
+  loadUser: () => Promise<any>
+}
