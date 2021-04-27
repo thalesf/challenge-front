15752bc7946ce3e944f365a5b2fbc39b51884884
Thales <thales.ferreira@volanty.com>
2021-04-27 15:48:00 -0300

    add: global style with Montserrat font

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Home from "@/app/pages/home";
-import Person from '@/app/pages/person';
+import Person from "@/app/pages/person";
+import "@/app/styles/global.scss";
 
 ReactDom.render(
   <BrowserRouter>
