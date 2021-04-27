9c263f12f4644d0d12cfc64d2c67075aa8b7681f
Thales <thales.ferreira@volanty.com>
2021-04-27 13:07:51 -0300

    Add: base config application

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Iclinic</h1>
+    </div>
+  )
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, "/public/js"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    port: 3000,
+    watchContentBase: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: "./public/index.html" })
+  ],
+};
