5aff7872540157fac4b97582793a919dde56de72
Thales <thales.ferreira@volanty.com>
2021-04-29 13:29:14 -0300

    fix: refactor person, function and enum

--- src/app/pages/person/person.tsx
+++ src/app/pages/person/person.tsx
@@ -11,6 +11,11 @@ import classes from "./person.scss";
 const darthVader = 'https://res.cloudinary.com/dnv46kilg/image/upload/v1619702852/darth-vader_eduahv.png';
 const lukeSkywalker = 'https://res.cloudinary.com/dnv46kilg/image/upload/v1619702852/luke-skywalker_xwkieq.png';
 
+enum User{
+  lukeSkywalker = 1,
+  darthVader = 4,
+}
+
 const Person: React.FC = () => {
 
   const dispatch = useDispatch();
@@ -22,16 +27,16 @@ const Person: React.FC = () => {
     dispatch(loadUsers());
   }, []);
 
-  const validateUser = () => !loading && user.user.url.includes("4");
+  const isDarthVader = () => !loading && user.user.url.includes(User.darthVader);
 
   return (
-    <div className={clsx(validateUser() ? classes.backgroundPrimary : classes.backgroundSecondary)}>
-      <Back userType={validateUser()} />
+    <div className={clsx(isDarthVader() ? classes.backgroundPrimary : classes.backgroundSecondary)}>
+      <Back userType={isDarthVader()} />
       <div className={classes.content}>
         <button className={clsx({
           [classes.button]: true,
           [classes.opacity]: loading,
-          [classes.buttonSecondary]: !validateUser()
+          [classes.buttonSecondary]: !isDarthVader()
         })} onClick={() => dispatch(loadUsers())} disabled={loading}>
           {loading ? 'Loading...' : 'choose your path again, Padawan'}
         </button>
@@ -39,11 +44,11 @@ const Person: React.FC = () => {
           {loading ? (
             <h1>Loading....</h1>
           ) : (
-            <Thumbnail src={validateUser() ? darthVader : lukeSkywalker} alt={validateUser() ? "Darth Vader" : "Luke Skywalker"} />
+            <Thumbnail src={isDarthVader() ? darthVader : lukeSkywalker} alt={isDarthVader() ? "Darth Vader" : "Luke Skywalker"} />
           )}
 
           {!loading ? (
-            <h1 className={clsx(validateUser() ? false : classes.fontColorPrimary, classes.title)}>
+            <h1 className={clsx(isDarthVader() ? false : classes.fontColorPrimary, classes.title)}>
               Your master is <strong className={classes.masterName}>{user.user.name}</strong></h1>
           ) : null}
 
